4eea2096c77a2c4837dd194f677e10c9810ccb6b
ntkurapati <ntkurapati@gmail.com>
2015-02-15 21:08:59 -0500

    fixes

--- smartdedupe.py
+++ smartdedupe.py
@@ -1,18 +1,15 @@
 __author__ = 'scornflakes'
 #!/usr/bin/env python2
 
-#packages required: yaml, beautifulsoup4
-
 import datetime
 import hashlib
 import ConfigParser
 import socket
 import os
 import sys
-from sqlalchemy import *
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-#from sqlalchemy.orm import relation, sessionmaker
-from sqlalchemy.orm import  sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 COMPUTER_NAME = socket.gethostname()
 PATH_SEP = os.path.sep
@@ -38,7 +35,7 @@ config.read(settings_file)
 db_engine = config.get("Database", 'db_engine')
 print db_engine
 
-engine = create_engine(db_engine)
+engine = sqlalchemy.create_engine(db_engine)
 Base = declarative_base(bind=engine)
 
 def md5string(str):
@@ -66,8 +63,8 @@ def md5(file_path):
 
 class Computer(Base):
     __tablename__ = 'computers'
-    computer_id = Column(Integer, autoincrement=True, primary_key=True)
-    computer_name = Column(String(270), nullable=False)
+    computer_id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, primary_key=True)
+    computer_name = sqlalchemy.Column(sqlalchemy.String(270), nullable=False)
 
     def __init__(self, computer_name):
         self.computer_name = computer_name
@@ -75,15 +72,16 @@ class Computer(Base):
 
 class Folder(Base):
     __tablename__ = 'folders'
-    folder_id = Column(Integer, autoincrement=True, primary_key=True)
-    folder_name = Column(String(270), nullable=False)
-    path = Column(String(270), nullable=False)
-    path_is_truncated = Column(Boolean)
-    parent_id = Column(Integer)
-    computer_id = Column(String(20))
-    last_modified = Column(DateTime)
-    last_checked = Column(DateTime)
-    md5_hash = Column(String(32), nullable=True)
+    folder_id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, primary_key=True)
+    folder_name = sqlalchemy.Column(sqlalchemy.String(270), nullable=False)
+    path = sqlalchemy.Column(sqlalchemy.String(800), nullable=False)
+    shrunk_path = sqlalchemy.Column(sqlalchemy.String(270))
+    path_is_truncated = sqlalchemy.Column(sqlalchemy.Boolean)
+    parent_id = sqlalchemy.Column(sqlalchemy.Integer)
+    computer_id = sqlalchemy.Column(sqlalchemy.String(20))
+    last_modified = sqlalchemy.Column(sqlalchemy.DateTime)
+    last_checked = sqlalchemy.Column(sqlalchemy.DateTime)
+    md5_hash = sqlalchemy.Column(sqlalchemy.String(32), nullable=True)
 
     def __init__(self, computer_id, parent_id, full_path):
         global PATH_SEP
@@ -91,42 +89,38 @@ class Folder(Base):
         self.parent_id=parent_id
         path_parts = full_path.split(PATH_SEP)
         self.folder_name = path_parts[-1]
-        path = path_parts[0:-1]
+        # path = path_parts[0:-1]
 
-        self.path = "{"+parent_id+"}"+PATH_SEP+self.folder_name
+        self.path = full_path
+        #self.shrunk_path = ("{%d}" % self.folder_id) +PATH_SEP+self.folder_name
 
-        """
-        if len(path) >= 270:
-            s.query(Folder).filter(Folder.path)
-            self.path_is_truncated = True
-            self.path = "{"+parent_id+"}"+PATH_SEP+self.folder_name
-        else:
-            self.path=path
-        """
 
     def get_full_path(self):
         global PATH_SEP
         if self.path_is_truncated:
             return PATH_SEP.join(s.query(Folder).get(self.parent_id).get_full_path(), self.folder_name)
         else:
-            return PATH_SEP.join(self.path)
+            return self.path
 
 
 class File(Base):
     __tablename__ = 'files'
-    file_id = Column(Integer, autoincrement=True, primary_key=True)
-    file_name = Column(String(270), nullable=False)
-    computer_id = Column(String(20), nullable=False)
-    folder_id = Column(Integer, nullable=True)
-    md5_hash = Column(String(32), nullable=True)
-    last_modified = Column(DateTime)
-    last_checked = Column(DateTime)
+    file_id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, primary_key=True)
+    file_name = sqlalchemy.Column(sqlalchemy.String(270), nullable=False)
+    path = sqlalchemy.Column(sqlalchemy.String(800), nullable=False)
+    computer_id = sqlalchemy.Column(sqlalchemy.String(20), nullable=False)
+    folder_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    md5_hash = sqlalchemy.Column(sqlalchemy.String(32), nullable=True)
+    last_modified = sqlalchemy.Column(sqlalchemy.DateTime)
+    last_checked = sqlalchemy.Column(sqlalchemy.DateTime)
     folder = None
 
-    def __init__(self, computer_id, folder_id, file_name):
+    def __init__(self, computer_id, folder_id, path, file_name):
         self.computer_id = computer_id
         self.file_name = file_name
-        self.folder_id = self.folder_id
+        if self.folder_id != -1:
+            self.folder_id = folder_id
+        self.path = path
         full_path = self.get_full_path()
         self.md5_hash = md5(full_path)
         self.last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(full_path))
@@ -134,20 +128,12 @@ class File(Base):
 
     def get_full_path(self):
         global PATH_SEP
-        return PATH_SEP.join(s.query(Folder).get(self.folder_id), self.file_name)
+        return PATH_SEP.join((self.path, self.file_name))
 
     def check_exists(self):
         self.last_checked = datetime.datetime.now()
         return os.path.exists(self.get_full_path())
 
-
-def find_directory(directory):
-    if not os.path.isdir(directory) or len(directory):
-        return None
-    if len(directory) < 270:
-        return s.query(Folder).filter(Folder)
-    else:
-        path_parts = directory.split(PATH_SEP)
 
 
 def get_computer_id():
@@ -165,26 +151,31 @@ def populate_db(computer_id, root_folder):
     entries = os.listdir(root_folder.get_full_path())
     for entry in entries:
         print entry
-        if os.path.isdir(entry):
-            folder = Folder(computer_id, root_folder.folder_id, entry)
-            matching_folder = s.query(Folder).filter(Folder.path == folder.path).first()
-            if matching_folder:
-                folder = matching_folder
-            else:
-                s.add(folder)
-                s.commit()
+        entry_full_path = PATH_SEP.join((root_folder.get_full_path(), entry))
+        try:
+            if os.path.isdir(entry_full_path) and os.access(entry_full_path, os.R_OK):
 
-            populate_db(computer_id, folder)
+                folder = Folder(computer_id, root_folder.folder_id, entry_full_path)
+                matching_folder = s.query(Folder).filter(Folder.path == folder.path).first()
+                if matching_folder:
+                    folder = matching_folder
+                else:
+                    s.add(folder)
+                    s.commit()
 
-        elif os.path.isfile(entry):
-            file2 = File(computer_id, root_folder.folder_id, entry)
-            matching_file = s.query(File)\
-                .filter(File.folder_id == file1.folder_id, File.file_name == file1.file_name).first()
-            if matching_file:
-                file2 = matching_file
-            else:
-                s.add(file2)
-                s.commit()
+                populate_db(computer_id, folder)
+
+            elif os.path.isfile(entry_full_path) and os.access(entry_full_path,os.R_OK):
+                file2 = File(computer_id, root_folder.folder_id, root_folder.get_full_path(),  entry)
+                matching_file = s.query(File)\
+                    .filter(File.folder_id == file2.folder_id, File.file_name == file2.file_name).first()
+                if matching_file:
+                    file2 = matching_file
+                else:
+                    s.add(file2)
+                    s.commit()
+        except WindowsError:
+            pass
 
 
 def remove_copies_in_same_directory(path):
@@ -193,10 +184,10 @@ def remove_copies_in_same_directory(path):
     from os.path import join
     for root, dirs, files in os.walk(path):
         for f in files:
-            file1 = File(get_computer_id(), root, f)
+            file1 = File(get_computer_id(), -1, root, f)
             existing_copy = s.query(File)\
                 .filter(File.md5_hash == file1.md5_hash)\
-                .filter(File.folder_id == file1.folder_id)\
+                .filter(File.path == file1.path)\
                 .filter(File.file_name != file1.file_name)\
                 .first()
             if existing_copy:
@@ -204,7 +195,7 @@ def remove_copies_in_same_directory(path):
                 print file1.md5_hash, existing_copy.MD5Hash
 
 
-def prune(directory):
+def prune(directory,  to_delete=False):
 ### does not identify copies in same dir!!
 
     print 'to prune:'
@@ -212,25 +203,20 @@ def prune(directory):
     for root, dirs, files in os.walk(directory):
         for f in files:
 
-            file = File(get_computer_id(), root, f)
-            existingcopy = s.query(File).filter(File.md5_hash== file.md5_hash).filter(File.FullPath!=file.FullPath).first()
-            if  existingcopy and (directory not in existingcopy.FullPath):
+            file = File(get_computer_id(), -1,  root, f)
+            existingcopy = s.query(File).filter(File.md5_hash== file.md5_hash).filter(File.FullPath != file.path).first()
+            if existingcopy and (directory not in existingcopy.FullPath):
                 print file.FullPath, existingcopy.FullPath,
-                print  file.md5_hash, existingcopy.MD5Hash
-                #os.remove(file.FullPath)
+                print file.md5_hash, existingcopy.MD5Hash
+                if to_delete:
+                    os.remove(file.get_full_path())
 
 def main(argv):
 
-    try:
-        #populate_db(directory1)
-        #populate_db(directory2)
-        #populate_db(directory3)
-        pass
-    except (Exception):
-        pass
-    remove_copies_in_same_directory(directory3)
-    prune(directory3)
-
+    root_folder = Folder(get_computer_id(), -1, 'D:\\MATLAB_stuff')
+    #s.add(root_folder)
+    #s.commit()
+    populate_db(get_computer_id(), root_folder)
 
 
 
