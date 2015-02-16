6e0100cbc81ddb96cd12921329f6744778531043
ntkurapati <ntkurapati@gmail.com>
2015-02-15 20:45:58 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='smartdedupe',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='scornflakes',
+    author_email='',
+    description='', requires=['sqlalchemy', 'mysql-python']
+)
+#mysql-connector c??
